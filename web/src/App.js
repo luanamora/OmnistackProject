9796505cc5e0025125e7c2ddb3f696b36e67a217
LuanaMora <luanamora32@gmail.com>
2020-01-28 15:45:42 -0300

    Arquivos CSS

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
-
-//Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-//Estado: Informações mantidas pelo componente (Lembrar: inutabilidade)
-//Propriedade: Informações que o componente PAI passa para o componente FILHO
-
+import React from 'react';
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
 function App() {
-  const [counter, setCounter] = useState(0);
 
-  function incrementCounter(){
-    setCounter(counter + 1);
-  }
   return (
-    <> 
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
