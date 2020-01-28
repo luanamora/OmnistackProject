84bf43dc4e3eff995535979377ed44b71e4b884b
LuanaMora <luanamora32@gmail.com>
2020-01-28 14:30:08 -0300

    Atualizações

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from './Header';
+
+//Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+//Estado
+//Propriedade: Informações que o componente PAI passa para o componente FILHO
 
 function App() {
   return (
-    <h1>Hello, World</h1>
+    <> 
+      <h1>Contador: 0</h1>
+      <button>Incrementar</button>
+    </>
   );
 }
 
-export default App;
+export default App; 
