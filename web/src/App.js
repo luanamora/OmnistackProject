d82344bf3be85c3490ae5aa0a20e5beb465f541b
LuanaMora <luanamora32@gmail.com>
2020-01-28 14:50:07 -0300

    Atualizações

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
 
 //Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-//Estado
+//Estado: Informações mantidas pelo componente (Lembrar: inutabilidade)
 //Propriedade: Informações que o componente PAI passa para o componente FILHO
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter(){
+    setCounter(counter + 1);
+  }
   return (
     <> 
-      <h1>Contador: 0</h1>
-      <button>Incrementar</button>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
     </>
   );
 }
