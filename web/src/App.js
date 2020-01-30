3b59de4d439809f8a5574daa14c435a0c0cf5f11
LuanaMora <luanamora32@gmail.com>
2020-01-30 14:41:59 -0300

    Geolocation

--- web/src/App.js
+++ web/src/App.js
@@ -1,34 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
+
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
+            <input
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required/>
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
+              <input 
+                type="number"
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
+
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
+              <input 
+                type="number"
+                name="longitude"
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
           <button type="submit">Salvar</button>
@@ -36,7 +80,7 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img src="https://avatars2.githubusercontent.com/u/42422551?s=460&v=4" alt="Luana Mora"></img>
               <div className="user-info">
@@ -48,7 +92,7 @@ function App() {
             <a href="https://github.com/luanamora">Acessar Perfil no Github</a>
           </li>
 
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img src="https://avatars2.githubusercontent.com/u/42422551?s=460&v=4" alt="Luana Mora"></img>
               <div className="user-info">
@@ -60,7 +104,7 @@ function App() {
             <a href="https://github.com/luanamora">Acessar Perfil no Github</a>
           </li>
 
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img src="https://avatars2.githubusercontent.com/u/42422551?s=460&v=4" alt="Luana Mora"></img>
               <div className="user-info">
@@ -72,7 +116,7 @@ function App() {
             <a href="https://github.com/luanamora">Acessar Perfil no Github</a>
           </li>
 
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img src="https://avatars2.githubusercontent.com/u/42422551?s=460&v=4" alt="Luana Mora"></img>
               <div className="user-info">
