551ca4bc3265a01a3e40cce851530cdd5c93b4f3
LuanaMora <luanamora32@gmail.com>
2020-02-03 15:57:29 -0300

    Atualizações do mobile

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello, Omnistack</Text>
+      <Text style={styles.title}>Hello, World</Text>
     </View>
   );
 }
