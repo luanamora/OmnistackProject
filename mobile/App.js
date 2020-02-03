1e8f8d4078ad65cb1dad13105e58c08eb93876a8
LuanaMora <luanamora32@gmail.com>
2020-02-03 10:59:54 -0300

    Mobile

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello, Omnistack</Text>
     </View>
   );
 }
@@ -12,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    fontWeight: "bold",
+    backgroundColor: '#7159C1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    color: '#FFF',
+  }
 });
