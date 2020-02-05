8183a9c56c454722e477b426149885214763860d
LuanaMora <luanamora32@gmail.com>
2020-02-05 10:53:42 -0300

    Atualizações Mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
-
-function Main() {
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
     useEffect(() => {
-        async function loadInitialPosition(){
+        async function loadInitialPosition() {
             const { granted } = await requestPermissionsAsync();
-            if (granted){
+            if (granted) {
                 const { coords } = await getCurrentPositionAsync({
                     enableHighAccuracy: true,
                 });
@@ -27,24 +27,40 @@ function Main() {
         loadInitialPosition();
     }, []);
 
-    if (!currentRegion){
+    if (!currentRegion) {
         return null;
     }
 
-    return(
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -24.5803857, longitude: -53.509473 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/42422551?s=460&v=4' }}/>
-                <Callout >
-                    <View style={styles.callout}> 
-                        <Text style={styles.devName}>Luana Mora</Text>
-                        <Text style={styles.devBio}>Uma bio qualquer</Text>
-                        <Text style={styles.devTechs}>Java, ReactJs, React Native</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
-    ); 
+    return (
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -24.5803857, longitude: -53.509473 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/42422551?s=460&v=4' }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'luanamora' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Luana Mora</Text>
+                            <Text style={styles.devBio}>Uma bio qualquer</Text>
+                            <Text style={styles.devTechs}>Java, ReactJs, React Native</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -60,7 +76,7 @@ const styles = StyleSheet.create({
     },
     callout: {
         width: 260,
-    }, 
+    },
     devName: {
         fontWeight: 'bold',
         fontSize: 16,
@@ -72,6 +88,41 @@ const styles = StyleSheet.create({
     devTechs: {
         marginTop: 5,
     },
+    searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLef: 15,
+    },
+
 })
 
 export default Main;
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -13,7 +13,7 @@ const Routes = createAppContainer(
             },
         },
         Profile: {
-            screen: Profile,
+            screen: Profile, 
             navigationOptions: {
                 title: 'Perfil no Github'
             }
@@ -21,6 +21,7 @@ const Routes = createAppContainer(
     }, {
         defaultNavigationOptions: {
            headerTintColor: '#FFF',
+           headerBackTitleVisible: false,
            headerStyle: {
                backgroundColor: '#7D42E7',
            }, 
